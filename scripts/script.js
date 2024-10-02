48061271a6e13fd73a9bd82a2100c1e3602b4b8c
Filip <angello2@github.com>
2024-10-03 01:31:23 +0200

    updated readme

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,27 @@
-const container = document.querySelector('.container')
-const buttons = container.querySelectorAll('.rating-button')
-const submitButton = container.querySelector('.submit-button')
-const ratingCard = document.getElementById('rating-card')
-const thankYouCard = document.getElementById('thank-you-card')
-const thankYouMessage = document.getElementById('you-selected-text')
+const container = document.querySelector(".container");
+const buttons = container.querySelectorAll(".rating-button");
+const submitButton = container.querySelector(".submit-button");
+const ratingCard = document.getElementById("rating-card");
+const thankYouCard = document.getElementById("thank-you-card");
+const thankYouMessage = document.getElementById("you-selected-text");
 
 var rating;
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        // remove selected class from other buttons
-        buttons.forEach(btn => btn.classList.remove('rating-button-selected'))
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		// remove selected class from other buttons
+		buttons.forEach((btn) => btn.classList.remove("rating-button-selected"));
 
-        rating = button.dataset.rating
-        button.classList.add('rating-button-selected')
+		rating = button.dataset.rating;
+		button.classList.add("rating-button-selected");
 
-        submitButton.disabled = false;
-    })
-})
+		submitButton.disabled = false;
+	});
+});
 
 submitButton.addEventListener("click", () => {
-    ratingCard.classList.add('hidden-card')
-    thankYouCard.classList.remove('hidden-card')
+	ratingCard.classList.add("hidden-card");
+	thankYouCard.classList.remove("hidden-card");
 
-    thankYouMessage.textContent = `You selected ${rating} out of 5`
-})
+	thankYouMessage.textContent = `You selected ${rating} out of 5`;
+});
